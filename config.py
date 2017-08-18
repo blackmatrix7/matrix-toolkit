3bea894d8f655c468739c2cf38b670b298d33bd0
Matrix <codecolor@outlook.com>
2017-08-18 14:13:45 +0800

    完善配置文件模块的支持

--- config.py
+++ config.py
@@ -12,6 +12,10 @@ __author__ = 'blackmatrix'
 
 class ConfigMixin:
 
+    """
+    Config混合类，支持部分dict协议，实现以类似操作dict的方式操作配置文件。
+    """
+
     def __setattr__(self, key, value):
         raise AttributeError
 
@@ -35,10 +39,17 @@ class ConfigMixin:
 
 
 class BaseConfig(ConfigMixin):
+    """
+    配置文件基类
+    """
     pass
 
 
 class DefaultConfig(BaseConfig):
+
+    """
+    配置文件的具体实现，所有的配置项都必须是全部大写
+    """
 
     # DEBUG
     DEBUG = False
@@ -52,7 +63,12 @@ default = DefaultConfig()
 
 configs = {'default': default}
 
+# 读取配置文件的名称，在具体的应用中，可以从环境变量、命令行参数等位置获取配置文件名称
 config_name = 'default'
+
+# 对本地配置文件的支持，当项目根目录存在localconfig.py文件时
+# 优先从localconfig.py中读取配置，如果不存在读取config.py的配置。
+# localconfig.py 应该加入git的忽略文件
 try:
     import localconfig
     current_config = localconfig.configs[config_name]
