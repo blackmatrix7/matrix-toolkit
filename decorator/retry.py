5b0123da9f2e294fbefc16116e969191d951c53e
Matrix <codecolor@outlook.com>
2017-08-29 10:45:18 +0800

    重试装饰器，增加一个指定异常类型的参数

--- decorator/retry.py
+++ decorator/retry.py
@@ -24,7 +24,8 @@ def retry(max_retries: int =5, delay: int =0, step: int =0, exceptions: tuple =(
     :param step:  每次重试后延迟递增，单位秒
     :param exceptions:  触发重试的异常类型
     :param sleep_func:  实现延迟的方法，默认为time.sleep，在一些异步框架，如
-    tornado中，使用time.sleep会导致整个线程阻塞，可以传入自定义的方法来实现延迟。
+    tornado中，使用time.sleep会导致阻塞，可以传入自定义的方法来实现延迟。
+    自定义方法函数签名应类似time.sleep，接收一个参数，为延迟执行的时间。
     :return: 
     """
     def wrapper(func):
