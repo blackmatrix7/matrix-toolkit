e88d69a9c8c99bfe2a2d4c1892e7f92171fa62a4
Matrix <codecolor@outlook.com>
2017-09-07 15:35:28 +0800

    略微调整重试装饰器的实现及注释

--- decorator/retry.py
+++ decorator/retry.py
@@ -28,7 +28,7 @@ def retry(max_retries: int =5, delay: int =0, step: int =0, exceptions: tuple =B
     在一些异步框架，如tornado中，使用time.sleep会导致阻塞，可以传入自定义的方法来实现延迟。
     自定义方法函数签名应与time.sleep相同，接收一个参数，为延迟执行的时间。
     :param callback: 回调函数，函数签名应接收一个参数，每次出现异常时，会将异常对象传入。
-    可用于记录异常日志等。
+    可用于记录异常日志，中断重试等。
     如回调函数正常执行，并返回True，则终止重试，并将被装饰函数的异常冒泡抛出。
     如回调函数正常执行，没有返回值或返回除True以外的结果，则继续重试。
     如回调函数抛出异常，则终止重试，并将回调函数的异常冒泡抛出。
@@ -38,14 +38,14 @@ def retry(max_retries: int =5, delay: int =0, step: int =0, exceptions: tuple =B
         @wraps(func)
         def _wrapper(*args, **kwargs):
             nonlocal delay, step, max_retries, exceptions
-            func_ex = None
+            func_ex = RuntimeError
             while max_retries > 0:
                 try:
                     return func(*args, **kwargs)
                 except exceptions as ex:
-                    func_ex = ex
                     if callable(callback) and callback(ex) is True:
                         raise ex
+                    func_ex = ex
                 finally:
                     max_retries -= 1
                     if delay > 0 or step > 0:
