1d74b80ad3a9d7738120f9f2052c74afa462eaf4
Matrix <codecolor@outlook.com>
2017-09-30 23:43:50 +0800

    更新tornsql代码

--- tornsql/session.py
+++ tornsql/session.py
@@ -5,6 +5,8 @@
 # @Site: https://github.com/blackmatrix7
 # @File: session
 # @Software: PyCharm
+
+from config import current_config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -30,22 +32,29 @@ scope = Scope()
 class DataBase:
 
     def __init__(self, config=None, connect_str=None, echo=True, max_overflow=5, encoding='utf-8'):
-        config = config or {}
-        self.echo = config.get('DB_ECHO', echo)
-        self.encoding = config.get('DB_ENCODING', encoding)
-        self.connect_str = config.get('DB_CONNECT', connect_str)
-        self.max_overflow = config.get('DB_MAX_OVERFLOW', max_overflow)
-        self.engine = None
+        self.config = config or {}
+        self.echo = self.config.get('DB_ECHO', echo)
+        self.encoding = self.config.get('DB_ENCODING', encoding)
+        self.connect_str = self.config.get('DB_CONNECT', connect_str)
+        self.max_overflow = self.config.get('DB_MAX_OVERFLOW', max_overflow)
+        self._engine = None
 
     def create_engine(self):
-        if self.engine is None:
-            self.engine = create_engine(self.connect_str, echo=self.echo, max_overflow=self.max_overflow, encoding=self.encoding)
-        return self.engine
+        return create_engine(self.connect_str, echo=self.echo, max_overflow=self.max_overflow, encoding=self.encoding)
 
     @property
-    def scoped_session(self):
-        session = scoped_session(sessionmaker(autocommit=False, autoflush=True, expire_on_commit=False, bind=engine, scopefunc=scope.get))
-        return session
+    def engine(self):
+        if self._engine is None:
+            self._engine = self.create_engine()
+        return self._engine
+
+
+def create_db(engin):
+    return scoped_session(sessionmaker(autocommit=False, autoflush=True, expire_on_commit=False, bind=engine), scopefunc=scope.get)
+
+engine = DataBase(connect_str=current_config.WORKFLOW_DB_CONNECT).engine
+
+db = create_db(engine)
 
 
 if __name__ == '__main__':
