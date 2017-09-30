62924d1950ab4663930a2a52945988c01429d2fe
Matrix <codecolor@outlook.com>
2017-09-30 23:44:35 +0800

    更新tornsql代码

--- tornsql/session.py
+++ tornsql/session.py
@@ -3,7 +3,7 @@
 # @Time: 2017/9/27 下午10:32
 # @Author: BlackMatrix
 # @Site: https://github.com/blackmatrix7
-# @File: session
+# @File: session.py
 # @Software: PyCharm
 
 from config import current_config
